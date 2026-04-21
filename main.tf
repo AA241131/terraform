bcc44e997b1a1beed1f916e226561b4358e022f0
Andres <AA241131@fi365.ort.edu.uy>
2026-04-20 22:02:33 -0300

    correction syntaxis

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "nombre-resource" {
   ami                    = "ami-098e39bafa7e7303d"
   instance_type          = "t3.micro"
   key_name               = "key-pair-ssh"
-  vpn_security_group_ids = "sg-0efa0a996a948f66c"
+  vpc_security_group_ids = "sg-0efa0a996a948f66c"
   tags = {
     Name      = "test-terraform-ec2"
   }
