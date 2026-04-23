1e7420387cb96e10a053070e759b39bc8bc448be
Andres <AA241131@fi365.ort.edu.uy>
2026-04-22 21:43:07 -0300

    vpc and subnets

--- main.tf
+++ main.tf
@@ -10,11 +10,39 @@ variable "key-pair" {
   default = "vockey"
 }
 
-resource "aws_instance" "nombre-resource" {
+# Create a VPC
+resource "aws_vpc" "test-terraform-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+#Crear la subnet
+resource "aws_subnet" "vpc-subnet" {
+  vpc_id     = aws_vpc.test-terraform-vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "test-terraform-sg" {
+  name = "test-terraform-sg"
+  vpc-id = aws_vpc.test-terraform-vpc.id
+  ingress {
+    from port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "test-terraform-ec2" {
   ami                    = "ami-098e39bafa7e7303d"
   instance_type          = "t3.micro"
   key_name               = var.key-pair
-  vpc_security_group_ids = ["sg-0efa0a996a948f66c"]
+  vpc_security_group_ids = [aws_security_group.test-terraform-sg.id]
   tags = {
     Name      = "test-terraform-ec2"
   }
