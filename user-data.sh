2970e32b5cf56298585d96c7c0d68843cb010975
Andres <AA241131@fi365.ort.edu.uy>
2026-05-05 13:51:09 -0300

    tree

--- user-data.sh
+++ user-data.sh
@@ -3,6 +3,7 @@ yum update -y
 yum -y install yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 yum -y install terraform
+yum -y install tree
 yum -y install git
 git clone https://github.com/AA241131/terraform /home/ec2-user/repositorio
 chmod 400 /home/ec2-user/repositorio/labuser.pem
