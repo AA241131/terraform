b4580d258f8841ee6ab64e85371a8263a6e5ee0e
Andres <AA241131@fi365.ort.edu.uy>
2026-05-05 15:45:44 -0300

    prueba 1

--- main.tf
+++ main.tf
@@ -22,14 +22,16 @@ module "deploy-instance" {
   source = "./modules/custom-deploy"
   count = 1
   
+  #variables para el modulo
+  region = "us-east-1"
+  vpc_cidr   = "172.16.0.0/16"
+  AMI        = "ami-098e39bafa7e7303d"
 }
 
+#salida desde el modulo
 output "ec2-instance-id" {
 value = module.deploy-instance.0.ec2-instance-id
 }
 
-output "ec2-instance-dns" {
-value = module.deploy-instance.0.ec2-instance-dns
-}
 
 
